3eef437443b483b981267a76ecb3610f77ef9b8c
ScottBlyth <37386802+ScottBlyth@users.noreply.github.com>
2024-05-22 22:51:13 +1000

    Secondary Plot - maybe plot win rates instead?

--- server.R
+++ server.R
@@ -13,6 +13,11 @@ data <- read.csv("cricsheet_data.csv")
 
 data_counts <- read.csv("games_played.csv")
 
+data_runs <- data %>% 
+  reframe(batting=team.batting,runs=runs, match_type=match_type,year=format(as.Date(start.date, "%Y-%m-%d"), "%Y")) %>% 
+  group_by(batting, year,match_type) %>% summarise(runsAvg=mean(runs))
+
+
 get_location <- memoise(geocode_OSM)
 
 # data_games = data %>% 
@@ -46,22 +51,27 @@ worldMap <- map("world", fill = TRUE, plot = FALSE)
 # taken from week 4 moodle content
 splitNames <- strsplit(worldMap$names, ":") 
 firstPartNames <- lapply(splitNames, function(x) x[1]) 
-cpal = colorNumeric("RdBu",data_counts$n, reverse=TRUE)
+
 game_types = c("T20", "ODI", "Test")
 
 create_games_played_graph = function(team) {
   if(team != "World") {
     data_counts_team <- data_counts %>% filter(team_1==team)
     p <- ggplot(data_counts_team, aes(x=as.numeric(year.x),y=n,colour=match_type.x))+geom_point()+
-      labs(title=team, x="Year",  y="Matches Played")+expand_limits(y=0)
+      labs(title=team, x="Year",  y="Matches Played",colour="Match Type")+expand_limits(y=0)
     return(p)
   }
   p <- ggplot(data_counts,  aes(x=as.numeric(year.x),y=n,colour=match_type.x))+geom_smooth()+
-    labs(title=team, x="Year",  y="Matches Played")+expand_limits(y=0)
+    labs(title=team, x="Year",  y="Matches Played",colour="Match Type")+expand_limits(y=0)
   return(p)
 }
 
-
+create_avg_runs_graph = function(team) {
+  d_team <- data_runs %>% filter(batting==team)
+  p <- ggplot(d_team, aes(x=as.numeric(year),y=runsAvg,colour=match_type))+geom_smooth()+
+    labs(title=team, x="Year",  y="Average Runs",colour="Match Type")+expand_limits(y=0)
+  return(p)
+}
 
 counts_smaller <- data_counts %>%
                   group_by(team_1, lat,long) %>% reframe()
@@ -71,6 +81,11 @@ team_names <- counts_smaller$team_1
 game_graphs = counts_smaller$graphs
 team_long = counts_smaller$lat
 team_lat = counts_smaller$long
+
+d <- data_counts %>% group_by(team_1, year.x) %>% summarise(n=sum(n))
+cpal = colorNumeric("RdBu",d$n, reverse=TRUE)
+cpal_runs = colorNumeric("Blues", data_runs$runsAvg)
+
 
 
 shinyServer(function(input, output) {
@@ -82,8 +97,9 @@ shinyServer(function(input, output) {
   game_forms = reactive(to_vec(for(i in 1:3) if(form_bools()[i]) game_types[i]))
   
   test <- reactive(filter(data_counts, 
-                          abs(as.numeric(year.x)-as.numeric(format(input$year, "%Y")))<=1, 
-                          match_type.x %in% game_forms()))
+                          year.x == format(input$year, "%Y"), 
+                          match_type.x %in% game_forms()) %>% 
+                          group_by(team_1) %>% summarise(n=sum(n))) 
 
   games_played <- reactive(test()$n[match(firstPartNames, test()$team_1)])
   
@@ -97,9 +113,24 @@ shinyServer(function(input, output) {
                                        color = cpal(games_played()),
                                        popup = paste("Country", test()$team_1[match(firstPartNames, test()$team_1)], 
                                           "\nn: ", test()$n[match(firstPartNames, test()$team_1)])
-                                     ) %>% addLegend(pal=cpal, values=data_counts$n))
-
+                                     ) %>% addLegend(pal=cpal, values=d$n))
   
+   runs <- reactive(filter(data_runs, year==format(input$runsYear, "%Y"), 
+                    match_type %in% game_forms()) %>% group_by(batting) %>% summarise(n=mean(runsAvg)))
+   
+   runs_list <- reactive(runs()$n[match(firstPartNames, runs()$batting)])
+   output$runsMap <- renderLeaflet(leaflet(worldMap) %>% 
+                                     addProviderTiles("Esri.WorldGrayCanvas") %>%
+                                   addPolygons(
+                                     stroke = FALSE, 
+                                     smoothFactor = 0.2, 
+                                     fillOpacity = 1,
+                                     color = cpal_runs(runs_list()), 
+                                     popup=paste("Country", runs()$batting[match(firstPartNames, runs()$batting)], 
+                                                 "\nAverage Runs: ", runs()$n[match(firstPartNames, runs()$batting)])
+                                   ))
+   p2 <- reactive(create_avg_runs_graph(input$country)) 
+   output$runsGraph <- renderPlot(p2())
 })
 
 
--- ui.R
+++ ui.R
@@ -7,7 +7,8 @@ shinyUI(fluidPage(
   # Application title
   tags$head(
     tags$style(
-      ".title {color: green}"
+      ".title {color: green; font-size: 40px;}", 
+      ".subtitle {color: black; font-size: 20px;}"
     )
   ),
   headerPanel(h1("Evolution: How has Cricket Changed?", class="title", align="center")),
@@ -21,10 +22,23 @@ shinyUI(fluidPage(
     textInput("country", "Country", value="World")
     ,width=2),
     mainPanel(
-      splitLayout(leafletOutput("gamesMap",width = "100%", height = 400),
-                  plotOutput("country_plot", height="200px", width="100%")) 
+      h1("What Countries Play Cricket?", class="subtitle"),
+      splitLayout(leafletOutput("gamesMap",width = "100%", height = 300),
+                  plotOutput("country_plot", height="200px", width="100%")
+          )
         )
-      ))
-  
+      ),
+  h1("Batting Performance: Average Runs of Each Country by Year", class="subtitle"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("runsYear", "Year", as.Date("2003", "%Y"),as.Date("2023", "%Y"), as.Date("1", "%Y"), round=TRUE, timeFormat="%Y")
+    ,width=2),
+    mainPanel(
+      splitLayout(
+        leafletOutput("runsMap", width = "100%", height = 200),
+        plotOutput("runsGraph", height="200px", width="100%")
+      )
+    )
+  )
 )
-
+)
